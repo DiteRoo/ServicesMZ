62327ce20af0f22efe7a8e4ba9c6c50b63f6c21d
Sergio Pacheco <ch_e_ch_o@hotmail.com>
2020-08-25 12:55:27 -0400

    make it better

---
+++ creatTablaProveedor.sql
@@ -0,0 +1,7 @@
+CREATE Table Proveedor(
+	id Serial PRIMARY KEY,
+	nombre varchar(200),
+	telefono varchar(200),
+	direccion varchar(200),
+	correo varchar(200)
+);
---
+++ db_config.php
@@ -0,0 +1,6 @@
+<?php
+define('DB_USER', "xkrefaigborerm"); // Usuario
+define('DB_PASSWORD', "aea4670c57abb164e0b3bc596b223abab717613e5c4448ad7fc58d871f95a80a"); // Password
+define('DB_DATABASE', "d7t9budmet8774"); // Nombre de la base de datos
+define('DB_SERVER', "ec2-34-236-215-156.compute-1.amazonaws.com"); // db server
+?>
