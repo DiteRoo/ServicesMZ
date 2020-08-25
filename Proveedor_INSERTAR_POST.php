bcd2ab3d4701de0fa388d13a51added3a26c1e88
Sergio Pacheco <ch_e_ch_o@hotmail.com>
2020-08-25 18:21:54 -0400

    make it better

--- Proveedor_INSERTAR_POST.php
+++ Proveedor_INSERTAR_POST.php
@@ -3,33 +3,33 @@
 
 	if($_SERVER['REQUEST_METHOD']=='POST'){
 		try{
-			$datos = json_decode(file_get_contents("php://input"),true);
-
-            #$id = $datos["id"];
-            $nombre = $datos["nombre"]; // obtener parametros POST
-            $telefono = $datos["telefono"];
+            $datos = json_decode(file_get_contents("php://input"),true);
+			//$codigo = $datos["codigo"];
+			$nombre = $datos["nombre"];
+			$telefono= $datos["telefono"];
             $direccion = $datos["direccion"];
-            $correo= $datos["correo"];
-
-            $respuesta = SQLGlobal::cudFiltro(
-				"INSERT INTO Proveedor VALUES (?,?,?,?)",
+            $correo = $datos["correo"];  
+			//$id = $datos["id"]; // obtener parametros POST
+			//$respuesta = SQLGlobal::query("QUERY");//sin filtro ("No incluir filtros ni '?'")
+			$respuesta = SQLGlobal::cudFiltro(
+				"INSERT INTO Proveedor (nombre,telefono,direccion,correo) VALUES (?,?,?,?)",
 				array($nombre,$telefono,$direccion,$correo)
 			);//con filtro ("El tamaño del array debe ser igual a la cantidad de los '?'")
-            if($respuesta>0){
-                echo json_encode(array(
-                    'respuesta'=>'200',
-                    'estado' => 'Se inserto correctamente el producto',
-                    'data'=>'El numero de registros afectados es: '.$respuesta,
-                    'error'=>''
-                ));
-            }else{
-                echo json_encode(array(
-                    'respuesta'=>'100',
-                    'estado' => 'No se inserto correctamente el producto.',
-                    'data'=>'El numero de registros afectados es: '.$respuesta,
-                    'error'=>''
-                ));
-            }
+			if($respuesta > 0){
+				 echo json_encode(array(
+				 'respuesta'=>'200',
+				 'estado' => 'Se inserto correctamente el producto',
+				 'data'=> 'El numero de registros afectados es: ' .$respuesta,
+				 'error'=>''
+				 ));
+		    }else{
+				echo json_encode(array(
+					'respuesta'=>'100',
+					'estado' => 'No se inserto correctamente el producto',
+					'data'=> 'El numero de registros afectados es: ' .$respuesta,
+					'error'=>''
+				 ));
+			}
 		}catch(PDOException $e){
 			echo json_encode(
 				array(
