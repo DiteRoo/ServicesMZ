3c82bbd9812df8ff36a47ba8722130da99ebb7a3
Sergio Pacheco <ch_e_ch_o@hotmail.com>
2020-08-25 14:51:14 -0400

    make it better

--- Proveedor_ADD.php
+++ Proveedor_ADD.php
@@ -1,3 +1,39 @@
 <?php
-	echo "hola mundo";
+	require 'SQLGlobal.php';
+
+	if($_SERVER['REQUEST_METHOD']=='POST'){
+		try{
+            $datos = json_decode(file_get_contents("php://input"),true);
+            
+            //$codigo = $datos["id"]
+            $nombre = $datos["nombre"]
+			$telefono = $datos["telefono"]
+			$direccion = $datos["direccion"]
+			$correo = $datos["correo"]
+
+			//$id = $datos["id"]; // obtener parametros GET
+			//$respuesta = SQLGlobal::query("QUERY");//sin filtro ("No incluir filtros ni '?'")
+			$respuesta = SQLGlobal::cudFiltro(
+				"INSERT INTO Proveedor Values(?,?,?,?)",
+				array($nombre, $telefono, $direccion, $correo)
+			);//con filtro ("El tamaño del array debe ser igual a la cantidad de los '?'")
+            if($respuesta > 0){
+                echo json_encode(array(
+                    'respuesta'=>'200',
+                    'estado' => 'Se insertó correctamente al proveedor',
+                    'data'=>'El numero de registros afectados es: '.$respuesta,
+                    'error'=>''
+                ));
+            }
+		}catch(PDOException $e){
+			echo json_encode(
+				array(
+					'respuesta'=>'-1',
+					'estado' => 'Ocurrio un error, intentelo mas tarde',
+					'data'=>'',
+					'error'=>$e->getMessage())
+			);
+		}
+	}
+
 ?>
