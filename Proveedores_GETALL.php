acab9cb1440b84ee743bb6467e36667bea8b9121
Sergio Pacheco <ch_e_ch_o@hotmail.com>
2020-08-25 12:57:15 -0400

    make it better

--- Proveedores_GETALL.php
+++ Proveedores_GETALL.php
@@ -4,7 +4,7 @@
 	if($_SERVER['REQUEST_METHOD']=='GET'){
 		try{
 			//$id = $_GET['id']; // obtener parametros GET
-			$respuesta = SQLGlobal::selectArray("select * FROM proveedores");//sin filtro ("No incluir filtros ni '?'")
+			$respuesta = SQLGlobal::selectArray("select * FROM proveedor");//sin filtro ("No incluir filtros ni '?'")
 			echo json_encode(array(
 				'respuesta'=>'200',
 				'estado' => 'Se obtuvieron los datos correctamente',
