cfbf4bb7bb0e10887929d476baf74452b66cd3a8
Sergio Pacheco <ch_e_ch_o@hotmail.com>
2020-08-25 17:18:07 -0400

    make it better

--- Proveedor_INSERTAR_POST.php
+++ Proveedor_INSERTAR_POST.php
@@ -5,14 +5,15 @@
 		try{
 			$datos = json_decode(file_get_contents("php://input"),true);
 
-			$nombre = $datos["nombre"]; // obtener parametros POST
+            $id = $datos["id"]
+            $nombre = $datos["nombre"]; // obtener parametros POST
             $telefono = $datos["telefono"];
             $direccion = $datos["direccion"];
             $correo= $datos["correo"];
 
             $respuesta = SQLGlobal::cudFiltro(
-				"INSERT INTO proveedor Values(?,?,?,?)",
-				array($nombre,$telefono,$direccion,$correo)
+				"INSERT INTO proveedor Values(?,?,?,?,?)",
+				array($id, $nombre, $telefono,$direccion,$correo)
 			);//con filtro ("El tamaño del array debe ser igual a la cantidad de los '?'")
             if($respuesta>0){
                 echo json_encode(array(
